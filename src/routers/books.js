6edbae1e9869c0e8f724e9cb7562699c0a82faeb
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-31 17:53:50 +0200

    finished post request for books

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
+/* GENERAL GET REQUEST */
 // router.get("/", async (req, res) => {
 //   const query = "SELECT * FROM books;";
 //   const result = await db.query(query);
 //   res.json({ books: result.rows });
 // });
 
+/* GET REQUEST ACCORDING TO SPECIFIC ID*/
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const query = `SELECT * FROM books WHERE id=${id}`;
@@ -15,6 +17,8 @@ router.get("/:id", async (req, res) => {
   res.json({ book: result.rows[0] });
 });
 
+/* GET REQUEST ACCORDING TO TYPE*/
+// url: (e.g) http://localhost:3030/books?type=Fiction
 router.get("/", async (req, res) => {
   const type = req.query.type;
   console.log("This is type: ", type);
@@ -24,10 +28,19 @@ router.get("/", async (req, res) => {
   res.json({ books: result.rows });
 });
 
-router.get("/?type=non_fiction", async (req, res) => {
-  const query = `SELECT * FROM books WHERE type='Non-Fiction'`;
-  const result = await db.query(query);
-  res.json({ books: result.rows });
+/* POST REQUEST */
+router.post("/", async (req, res) => {
+  const newBook = req.body;
+  console.log("this is newBook: ", newBook);
+  const { title, type, author, topic, publicationDate, pages } = newBook;
+
+  const query = `INSERT INTO books
+  (title, type, author, topic, publicationDate, pages)
+  VALUES('${title}', '${type}', '${author}', '${topic}', '${publicationDate}', '${pages}');`;
+
+  const addNewBook = await db.query(query);
+
+  res.json({ books: addNewBook.rows });
 });
 
 module.exports = router;
